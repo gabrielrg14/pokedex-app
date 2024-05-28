cd07b8e885096199fde045949c2edbe6f46f4060
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2024-05-28 00:37:48 -0300

    Apply prettier rules to Jest configuration files

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,15 +1,15 @@
-import "@testing-library/react-native/extend-expect";
+import "@testing-library/react-native/extend-expect"
 import "./src/test/server/setup-env"
 
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock"
+jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage)
 
-jest.mock('@react-navigation/native', () => {
+jest.mock("@react-navigation/native", () => {
   return {
     useNavigation: () => {
       return {
-        navigate: jest.fn()
-      };
+        navigate: jest.fn(),
+      }
     },
-  };
-});
+  }
+})
--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,9 @@
-import type { Config } from 'jest';
+import type { Config } from "jest"
 
 const config: Config = {
   preset: "jest-expo",
-  setupFilesAfterEnv: ['./jest-setup.ts'],
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native)|expo|@expo)"
-  ]
-};
+  setupFilesAfterEnv: ["./jest-setup.ts"],
+  transformIgnorePatterns: ["node_modules/(?!((jest-)?react-native|@react-native)|expo|@expo)"],
+}
 
-export default config;
+export default config
